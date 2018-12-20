e22b5989ecfef17fb977912eafb87b67d4c55be6
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2018-12-19 20:41:32 -0600

    Updated both blog and Project pages

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php get_header(); ?>
-<header>
-    <div class="text-center container">
-        <h1>Welcome to my Blog</h1>
+<header class="jumbotron jumbotron-fluid" id="header">
+    <div class="d-flex justify-content-center heading">
+        <img class="title-image" src="<?php echo get_template_directory_uri() . '/img/code-1839406_1920.jpg' ?>" alt="Coding">
+        <h1 id="centered" class="blog-title"><?php echo single_cat_title(); ?></h1>
     </div>
 </header>
 <div class="container">
@@ -10,6 +11,9 @@
         the_post(); ?>
     <div>
         <h2 class="blog-posts"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    </div>
+    <div>
+        <?php the_excerpt(); ?>
     </div>
     <?php 
 } ?>
