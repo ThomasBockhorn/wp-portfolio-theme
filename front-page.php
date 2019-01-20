73696896ad33bbfd4cfcec22ecdd78a25f0b411e
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2019-01-20 01:26:11 -0600

    updated some style

--- front-page.php
+++ front-page.php
@@ -13,16 +13,16 @@
     <h1 class="display-4 d-flex justify-content-center heading">Design</h1>
     <div class="container justify-content-center">
       <div class="row" id="design-maxim">
-        <div class="col-lg-2" id="terminal"><i class="fas fa-terminal"></i></div>
-        <div class="col-lg-10" id="maxim"><p>Websites need a central simple focus.<p></div>
+        <div class="col-sm-2" id="terminal_icon"><i class="fas fa-terminal"></i></div>
+        <div class="col-sm-10" id="maxim"><p>Websites need a central simple focus.<p></div>
       </div>
       <div class="row" id="design-maxim">
-        <div class="col-lg-2" id="terminal"><i class="fas fa-terminal"></i></div>
-        <div class="col-lg-10" id="maxim"><p>Visuals are greatly enhanced by empty space.</p></div>
+        <div class="col-sm-2" id="terminal_icon"><i class="fas fa-terminal"></i></div>
+        <div class="col-sm-10" id="maxim"><p>Visuals are greatly enhanced by empty space.</p></div>
       </div>
       <div class="row" id="design-maxim">
-        <div class="col-lg-2" id="terminal"><i class="fas fa-terminal"></i></div>
-        <div class="col-lg-10" id="maxim"><p>The careful use of color brings emotions that engage the audience.</p></div>
+        <div class="col-sm-2" id="terminal_icon"><i class="fas fa-terminal"></i></div>
+        <div class="col-sm-10" id="maxim"><p>The careful use of color brings emotions that engage the audience.</p></div>
       </div>
     </div>
   </div>
