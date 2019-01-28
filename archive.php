882a3678fd02d836b9d9c2fdaae3c206989e9a5e
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2019-01-27 22:09:18 -0600

    Updated title

--- archive.php
+++ archive.php
@@ -2,7 +2,7 @@
 <header class="jumbotron jumbotron-fluid" id="header">
     <div class="d-flex justify-content-center heading">
         <img class="title-image" src="<?php echo get_template_directory_uri() . '/img/boat-1992136_1920.jpg' ?>" alt="Row boat in still water">
-        <h1 id="centered" class="blog-title">Welcome to <?php echo single_cat_title(); ?></h1>
+        <h1 id="centered" class="blog-title"><?php echo single_cat_title(); ?></h1>
     </div>
 </header>
 <div class="container">
